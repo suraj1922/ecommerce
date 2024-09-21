e21ec9b2d560e79fd564590e8074742339a2e753
SurajGupta <surajgupta19224@gmail.com>
2024-09-21 20:43:39 +0530

    category

--- src/Components/Pages/Listing/index.js
+++ src/Components/Pages/Listing/index.js
@@ -1,14 +1,80 @@
 import React from 'react'
 import Sidebar from '../../Sidebae'
+import Banner from '../../../images/slideBanner1.jpg'
+import Button from '@mui/material/Button'
+import { IoIosMenu } from 'react-icons/io'
+import { TfiLayoutGrid4Alt } from 'react-icons/tfi'
+import { TbGridDots } from 'react-icons/tb'
+import { HiViewGrid } from 'react-icons/hi'
+import { FaAngleDown } from 'react-icons/fa'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+// import ProductItem from '../../../Components/ProductItem'
+
 const Listing = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [productView, setProductView] = useState('four')
+  const openDropDown = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <section className="product_Listing_Page">
         <div className="container">
           <div className='productListing d-flex'>
-            <Sidebar/>
+            <Sidebar />
             <div className='content_right'>
-              
+              <img src={Banner} alt="" style={{ borderRadius: '8px' }} />
+
+              <div className='showBy mt-3 mb-3 d-flex align-items-center'>
+                <div className='btnWrapper d-flex align-item-center '>
+                  <Button className={productView==='one' && 'act'} onClick={()=>setProductView('one')}><IoIosMenu /></Button>
+                  <Button className={productView==='three' && 'act'} onClick={()=>setProductView('three')}><TbGridDots /></Button>
+                  <Button className={productView==='four' && 'act'} onClick={()=>setProductView('four')}><TfiLayoutGrid4Alt /></Button>
+
+                </div>
+                <div className='ml-auto showByFilter'>
+                  <Button onClick={handleClick}>Show 9 <FaAngleDown /></Button>
+                  <Menu
+                    className='w-100 showPerPage'
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={openDropDown}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      'aria-labelledby': 'basic-button',
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>9</MenuItem>
+                    <MenuItem onClick={handleClose}>18</MenuItem>
+                    <MenuItem onClick={handleClose}>27</MenuItem>
+                    <MenuItem onClick={handleClose}>45</MenuItem>
+                  </Menu>
+                </div>
+              </div>
+
+              <div className='productListing'>
+                {/* <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/>
+                <ProductItem itemView={productView}/> */}
+              </div>
+
+
+
+              <div className='d-flex align-item-center justify-content-center mt-5'>
+              <Pagination count={10} color="primary" size='large' />
+              </div>
             </div>
           </div>
         </div>
--- src/Components/Sidebae/index.js
+++ src/Components/Sidebae/index.js
@@ -5,108 +5,110 @@ import Checkbox from '@mui/material/Checkbox';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css'
 import { Link } from 'react-router-dom';
-import slideBanner1 from '../../images/slideBanner1.jpg';
+import slideBanner1 from '../../images/banner1.jpg';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Sidebar = () => {
-  const [value, setValue] = useState([100,60000])
+  const [value, setValue] = useState([100, 60000])
   const [value2, setValue2] = useState(0);
   return (
     <>
       <div className='sidebar'>
-        <div className='filterBox'>
-          <h6>PRODUCT CATEGORIES</h6>
-          <div className='scroll'>
-            <ul>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Women" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Beauty" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Kids" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Women" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Beauty" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Kids" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
-              </li>
-            </ul>
+        <div className='sticky'>
+          <div className='filterBox'>
+            <h6>PRODUCT CATEGORIES</h6>
+            <div className='scroll'>
+              <ul>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Women" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Beauty" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Kids" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Women" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Beauty" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Kids" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Men" />
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-        
-        <div className="filterBox">
-          <h6>FILTER BY Price</h6>
-          <RangeSlider value={value} onInput={setValue} min={100} max={60000} step={5}/>
 
-          <div className='d-flex pt-2 pb-2 priceRange'>
-            <span>From: <strong className='text-dark'>Rs: {value[0]}</strong></span>
-            <span className='ml-auto'> From: <strong className='text-dark'>Rs:{value[1]}</strong></span>
+          <div className="filterBox">
+            <h6>FILTER BY Price</h6>
+            <RangeSlider value={value} onInput={setValue} min={100} max={60000} step={5} />
+
+            <div className='d-flex pt-2 pb-2 priceRange'>
+              <span>From: <strong className='text-dark'>Rs: {value[0]}</strong></span>
+              <span className='ml-auto'> From: <strong className='text-dark'>Rs:{value[1]}</strong></span>
+            </div>
           </div>
-        </div>
 
-        <div className='filterBox'>
-          <h6>PRODUCT STATUS</h6>
-          <div className='scroll'>
-            <ul>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="In Stock" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="On Sale" />
-              </li>
-            </ul>
+          <div className='filterBox'>
+            <h6>PRODUCT STATUS</h6>
+            <div className='scroll'>
+              <ul>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="In Stock" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="On Sale" />
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
 
-        <div className='filterBox'>
-          <h6>Brands</h6>
-          <div className='scroll'>
-            <ul>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Frito Lay" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Nespresso" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Oreo" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Quaker" />
-              </li>
-              <li>
-                <FormControlLabel className="w-100" control={<Checkbox />} label="Welch's" />
-              </li>
-            </ul>
+          <div className='filterBox'>
+            <h6>Brands</h6>
+            <div className='scroll'>
+              <ul>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Frito Lay" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Nespresso" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Oreo" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Quaker" />
+                </li>
+                <li>
+                  <FormControlLabel className="w-100" control={<Checkbox />} label="Welch's" />
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
 
-        <br/>
-        <Link to ="#"><img src={slideBanner1} className='w-100'/></Link>
+          <br />
+          <Link to="#"><img src={slideBanner1} className='w-100' /></Link>
+        </div>
       </div>
     </>
   )
